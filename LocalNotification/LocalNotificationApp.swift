b0aa58f9410d6cb18718d03d0edaac993d3cef3b
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-11-04 19:06:33 +0700

    Initial Commit

---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by Đoàn Văn Khoan on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
